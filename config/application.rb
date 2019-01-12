09c4cdf352040513bd628f4ef6c02558a9d15052
Miguel Furtado <miguelmofurtado@gmail.com>
2019-01-12 17:04:16 +0000

    no message

--- config/application.rb
+++ config/application.rb
@@ -22,10 +22,10 @@ module PoliticandoBackend
       protocol: ENV['DEFAULT_URL_PROTOCOL'] || 'https'
     }
 
-    config.middleware.use "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
-        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options]
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
 end
