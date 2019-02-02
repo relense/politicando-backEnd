671d01a158673e523e8f500b7867564f7e553d87
Miguel Furtado <miguelmofurtado@gmail.com>
2019-02-02 11:46:55 +0000

    improved filter to associate a party to an article

--- app/models/article.rb
+++ app/models/article.rb
@@ -30,38 +30,35 @@ class Article < ApplicationRecord
 
     parties.each_with_index do |party, index|
 
-      if party.description === "LIVRE" 
+      if party.party_name === "CDS-PP"
+        party_count[index] = title.scan(/\s#{party.party_name}\W/).length #space in the beggining and end of word
+        party_count[index] += title.scan(/^#{party.party_name}\s/).length #beggining of the line word with space after
+
+        party_count[index] += title.scan(/\sCDS\W/).length #space in the beggining and end of word
+        party_count[index] += title.scan(/^CDS\s/).length #beggining of the line word with space after
+    
+        party_count[index] += title.scan(/\s#{party.description}\W/).length
+        party_count[index] += title.scan(/^#{party.description}\s/).length
+    
+        party_count[index] += content.scan(/\s#{party.party_name}\W/).length
+        party_count[index] += content.scan(/^#{party.party_name}\s/).length
+        
+        party_count[index] += content.scan(/\sCDS\W/).length
+        party_count[index] += content.scan(/^CDS\s/).length
+    
+        party_count[index] += content.scan(/\s#{party.description}\W/).length
+        party_count[index] += content.scan(/^#{party.description}\s/).length
+
+      elsif party.description === "LIVRE" 
         party_count[index] = title.scan(/\s#{party.description}\W/).length #space in the beggining and end of word
         party_count[index] += title.scan(/^#{party.description}\s/).length #beggining of the line word with space after
 
         party_count[index] += content.scan(/\s#{party.description}\W/).length
         party_count[index] += content.scan(/^#{party.description}\s/).length
-
-      elsif party.party_name === "MAS"
-        party_count[index] = title.scan(/\s#{"Partido " + party.party_name}\W/).length #space in the beggining and end of word
-        party_count[index] += title.scan(/^#{"Partido " + party.party_name}\s/).length #beggining of the line word with space after
-
-        party_count[index] += title.scan(/\s#{party.description}\W/).length
-        party_count[index] += title.scan(/^#{party.description}\s/).length
-
-        party_count[index] += content.scan(/\s#{"Partido " + party.party_name}\W/).length
-        party_count[index] += content.scan(/^#{"Partido " + party.party_name}\s/).length
-
-        party_count[index] += content.scan(/\s#{party.description}\W/).length
-        party_count[index] += content.scan(/^#{party.description}\s/).length
-
+      
+      
       elsif party.description === "Iniciativa Liberal"
-        party_count[index] = title.scan(/\s#{party.party_name}\W/).length #space in the beggining and end of word
-        party_count[index] += title.scan(/^#{party.party_name}\s/).length #beggining of the line word with space after
-
-        party_count[index] += title.scan(/\s#{"Partido " + party.description}\W/).length
-        party_count[index] += title.scan(/^#{"Partido " + party.description}\s/).length
-
-        party_count[index] += content.scan(/\s#{party.party_name}\W/).length
-        party_count[index] += content.scan(/^#{party.party_name}\s/).length
-
-        party_count[index] += content.scan(/\s#{"Partido " + party.description}\W/).length
-        party_count[index] += content.scan(/^#{"Partido " + party.description}\s/).length
+        party_count[index] = party_count_keyword_party_description(party, "Partido ")
 
       elsif party.description === "Aliança"
         party_count[index] = title.scan(/\s#{"O " + party.description}\W/).length
@@ -71,17 +68,17 @@ class Article < ApplicationRecord
         party_count[index] += content.scan(/^#{"O " + party.description}\s/).length
 
       else
-        party_count[index] = title.scan(/\s#{party.party_name}\W/).length #space in the beggining and end of word
-        party_count[index] += title.scan(/^#{party.party_name}\s/).length #beggining of the line word with space after
-
-        party_count[index] += title.scan(/\s#{party.description}\W/).length
-        party_count[index] += title.scan(/^#{party.description}\s/).length
-
-        party_count[index] += content.scan(/\s#{party.party_name}\W/).length
-        party_count[index] += content.scan(/^#{party.party_name}\s/).length
-
-        party_count[index] += content.scan(/\s#{party.description}\W/).length
-        party_count[index] += content.scan(/^#{party.description}\s/).length
+       party_count[index] = title.scan(/\s#{party.party_name}\W/).length #space in the beggining and end of word
+       party_count[index] += title.scan(/^#{party.party_name}\s/).length #beggining of the line word with space after
+   
+       party_count[index] += title.scan(/\s#{party.description}\W/).length
+       party_count[index] += title.scan(/^#{party.description}\s/).length
+   
+       party_count[index] += content.scan(/\s#{party.party_name}\W/).length
+       party_count[index] += content.scan(/^#{party.party_name}\s/).length
+   
+       party_count[index] += content.scan(/\s#{party.description}\W/).length
+       party_count[index] += content.scan(/^#{party.description}\s/).length
       end
     end
 
@@ -92,17 +89,17 @@ class Article < ApplicationRecord
       associated_parties = Partido.find(most_mentioned[0][1] + 1).party_name
       associated = true
 
-        if most_mentioned[1][0] >= 3
+        if most_mentioned[1][0] >= 1
           ArticlesPartido.create(partido_id: most_mentioned[1][1] + 1, article_id: self.id)
           associated_parties += "," + Partido.find(most_mentioned[1][1] + 1).party_name
         end
 
-        if most_mentioned[2][0] >= 3
+        if most_mentioned[2][0] >= 1
           ArticlesPartido.create(partido_id: most_mentioned[2][1] + 1, article_id: self.id)
           associated_parties += "," + Partido.find(most_mentioned[2][1] + 1).party_name
         end
 
-        if most_mentioned[3][0] >= 3
+        if most_mentioned[3][0] >= 1
           ArticlesPartido.create(partido_id: most_mentioned[3][1] + 1, article_id: self.id)
           associated_parties += "," + Partido.find(most_mentioned[3][1] + 1).party_name
         end
@@ -118,4 +115,22 @@ class Article < ApplicationRecord
     end
     return false
   end
+
+  def party_count_keyword_party_description(party, keyword)
+    count = 0
+
+    count = title.scan(/\s#{party.party_name}\W/).length #space in the beggining and end of word
+    count += title.scan(/^#{party.party_name}\s/).length #beggining of the line word with space after
+
+    count += title.scan(/\s#{keyword + party.description}\W/).length
+    count += title.scan(/^#{keyword + party.description}\s/).length
+
+    count += content.scan(/\s#{party.party_name}\W/).length
+    count += content.scan(/^#{party.party_name}\s/).length
+
+    count += content.scan(/\s#{keyword + party.description}\W/).length
+    count += content.scan(/^#{keyword + party.description}\s/).length
+  end
+
 end
+
